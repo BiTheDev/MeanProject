4e9692a82283a487026be8315827811020879488
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-09-24 18:29:33 -0700

    add get User

--- server/routes.js
+++ server/routes.js
@@ -4,8 +4,9 @@ function router(app){
     app.post("/api/User/new",api.Register);
     app.put("/api/User/newdate/:user1_id",api.CreateDate);
     app.get("/api/User/:id",api.GetUser);
-    app.get("/api/login/",api.Login)
-    app.put("/api/User2/:user2_id",api.UpdateUserDate);
+    app.get("/api/login/",api.Login);
+    app.get("/api/Users",api.GetUsers);
+    app.patch("/api/User2/:user2_id",api.UpdateUserDate);
     app.delete("/api/Date/Destroy/:id",api.RemoveDate);
 
 }
