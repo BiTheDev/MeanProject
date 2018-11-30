b10386f39c51d2a015790bbd3f4c23f97964bc67
Angie To <angie.to@berkeley.edu>
2018-11-29 18:08:52 -0800

    modified backend user registration

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -2,53 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
-  newUser = { 
-    firstname: "", 
-    email:"", 
-    password:"", 
-    gender:"", 
-    age:"", 
-    city:"" 
-  }
-  confirmpassword;
-  error;
-  constructor(
-    private _httpService: HttpService,
-    private _route: ActivatedRoute,
-    private _router: Router
-  ) { }
-
-  ngOnInit() {
-  }
-  Register(){
-    if(this.newUser.password != this.confirmpassword){
-      this.error = "Oops, your password didnt match, please try again!"
-    }else{
-      let obs = this._httpService.createUser(this.newUser);
-      obs.subscribe(data=>{
-        console.log("createUser server respons data:", data)
-        if(data['errors']){
-          console.log(data['error']); 
-        }else{
-          console.log("Create User success", data);
-          this.newUser = { 
-            firstname: "", 
-            email:"", 
-            password:"", 
-            gender:"", 
-            age:"", 
-            city:"" 
-          }
-          return this._router.navigate(['/dashboard/'+data['_id']])
-        }
-      })
-    
+    newUser = { 
+        firstname: '', 
+        email: '', 
+        password: '', 
+        gender: '', 
+        age: '', 
+        city: '' 
     }
-  }
+    confirmpassword;
+    error;
+    constructor(
+        private _httpService: HttpService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) { }
 
+    ngOnInit() {
+    }
+
+    // Error not displayed on the front-end
+    Register() {
+        if (this.newUser.password !== this.confirmpassword) {
+            this.error = 'Oops, your password didnt match, please try again!'
+        } else {
+            const obs = this._httpService.createUser(this.newUser);
+            obs.subscribe((newUser: any) => {
+                console.log('createUser server respons data:', newUser);
+                if (newUser['errors']) {
+                    console.log(newUser['error']); 
+                    this.error = newUser.message;
+                } else {
+                    console.log('Create User success', newUser);
+                    this.newUser = { 
+                        firstname: '', 
+                        email: '', 
+                        password: '', 
+                        gender: '', 
+                        age: '', 
+                        city: '' 
+                    };
+                    return this._router.navigate(['/dashboard/' + newUser['_id']]);
+                }
+            });
+        }
+    }
 }
